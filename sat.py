fb3acc2e6578bd63a13e1f17259c4005f0b2123d
radisa-bogdanovic <tictac992@gmail.com>
2023-12-19 20:13:23 +0100

    inicijalni-finalni commit

---
+++ sat.py
@@ -0,0 +1,21 @@
+
+
+import tkinter as sat
+import time
+
+def azuriraj_sat():
+    trenutno_vreme= time.strftime("%H:%M:%S")
+    satt.config(text=trenutno_vreme)
+    satt.after(1000, azuriraj_sat)
+
+app=sat.Tk()
+app.title("Digitalni sat")
+
+satt= sat.Label(app, text="", font=("Helvetica"), height=6, width=10 )
+satt.pack()
+
+
+azuriraj_sat()
+app.mainloop()
+
+
